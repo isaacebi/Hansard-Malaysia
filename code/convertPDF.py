5000d53cb6eed13b0e3d1c0063c2c5b400c0bd5a
isaacebi <isaac_ebi@rocketmail.com>
2022-11-05 22:32:56 +0800

    Update convertPDF.py

--- code/convertPDF.py
+++ code/convertPDF.py
@@ -10,40 +10,34 @@ def isExist(checkPath):
     if not isExist:
         os.mkdir(checkPath)
 
+def toPDF(filePath, toWrite):
+    with open(filePath, 'rb') as reader:
+        # create reader variable that will read the pdffileobj
+        pdfreader=PyPDF2.PdfFileReader(reader, strict=False)
+
+        # get total pages
+        totalPages = pdfreader.numPages
+
+        for curPages in range(totalPages):
+            pageObj = pdfreader.getPage(curPages)
+            text = pageObj.extractText()
+            toWrite.writelines(text)
+
 # %%
 PARENT_PATH = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 DATA_PATH = os.path.join(PARENT_PATH, 'data')
 PDF = os.path.join(DATA_PATH, 'PDF')
-TEST = os.path.join(PDF, 'hansard_14-03-k01-01.pdf')
 TEXT = os.path.join(DATA_PATH, 'TEXT')
 COMPILE_TEXT = os.path.join(TEXT, 'compile.txt')
-
-# %%
-# for head, body, file in os.walk(PDF):
-#     print(file)
 
 # %%
 # check or create path
 isExist(TEXT)
 
-with open(COMPILE_TEXT, 'w') as f:
-
-    #create file object variable
-    pdffileobj=open(TEST,'rb')
-    
-    #create reader variable that will read the pdffileobj
-    pdfreader=PyPDF2.PdfFileReader(pdffileobj)
-    
-    #This will store the number of pages of this pdf file
-    x=pdfreader.numPages
-    
-    #create a variable that will select the selected number of pages
-    pageobj=pdfreader.getPage(x+1)
-    
-    #(x+1) because python indentation starts with 0.
-    #create text variable which will store all text datafrom pdf file
-    text=pageobj.extractText()
-
-    f.writelines(text)
+with open(COMPILE_TEXT, 'w', encoding='utf-8') as f:
+    for head, body, files in os.walk(PDF):
+        for file in files:
+            file = os.path.join(head, file)
+            toPDF(file, f)
 
 # %%
